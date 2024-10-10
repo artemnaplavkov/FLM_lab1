cb03c4a5d7be3dcd7ce407d7e98d7b443c52d4d7
artemnaplavkov <74201877+artemnaplavkov@users.noreply.github.com>
2024-10-10 16:26:45 +0300

    lab.cpp

--- lab.cpp
+++ lab.cpp
@@ -26,11 +26,6 @@ int main(){
 		if (gram1[i] != '{' and gram1[i] != '}' and gram1[i] != ',')
 			gramG.push_back(gram1[i]);
 	}
-	for (int i = 0; i <= gramG.size()-1; i++) {
-		cout << gramG[i] << " ";
-	}
-	cout << "\n";
-
 
 	cout << "Insert grammatic P like this: {a,b,+,-} \n";
 	cin >> gram2;
@@ -38,19 +33,15 @@ int main(){
 		if (gram2[i] != '{' and gram2[i] != '}' and gram2[i] != ',')
 			gramP.push_back(gram2[i]);
 	}
-	for (int i = 0; i <= gramP.size()-1; i++) {
-		cout << gramP[i] << " ";
-	}
-	cout << "\n";
 
 
-	for (int i = 0; i <= 3; i++) {
+	for (int i = 0; i <= 1; i++) { //Менять
 		cout << "Insert " << i + 1 << " letter of grammar, by using ->  \n";
 		cin >> gram1;
 
-		if (gram1[1] != '-' || gram1[2] != '>' || (find(gramG.begin(), gramG.end(), gram1[1]) == gramG.end()) || (find(gramP.begin(), gramP.end(), gram1[4]) == gramP.end()))
+		if (gram1[1] != '-' || gram1[2] != '>')// || (find(gramG.begin(), gramG.end(), gram1[1]) == gramG.end()) || (find(gramP.begin(), gramP.end(), gram1[4]) == gramP.end()))
 		{
-			cout << "incorrect insert";
+			cout << "incorrect insert \n ";
 			i--;
 			continue;
 		}
@@ -66,81 +57,70 @@ int main(){
 		P = make_pair(GS, gram1[3]);
 		GPS.push_back(P);
 
-
 		
-		if (gram1[5] == '|' && !(!gram1[6]) && !gram1[7]) {
+		if (gram1[5] == '|' && (!(!gram1[6])) && (!gram1[7])) {
 			gramG.push_back('@');
 			GS = make_pair(gram1[0], '@');
-			P = make_pair(GS, gram1[5]);
-			GPS.push_back(P);
-		}
-		else if (gram1[5] == '|' && !(!gram1[6]) && !(!gram1[7])) {
-			GS = make_pair(gram1[0], gram1[7]);
 			P = make_pair(GS, gram1[6]);
 			GPS.push_back(P);
 		}
-			
-		
+		else if (gram1[5] == '|' && (!(!gram1[6])) && (!(!gram1[7]))) {
+			GS = make_pair(gram1[0], gram1[7]);
+			P = make_pair(GS, gram1[6]);
+			GPS.push_back(P);
+		}	
 	}
-
 
 	for (int i = 0; i <= GPS.size() - 1; i++) {
 		cout << GPS[i].first.first << " -> " << GPS[i].second <<  " -> " << GPS[i].first.second <<" \n";
 	}
 
-	for (int i = 0; i <= gramG.size(); i++) {
-		for (int j = 0; j <= (alphabet.size() - gramG.size()); i++) {
-			if (gramG[i] == alphabet[j])
-				alphabet.erase(alphabet[j]);
-		}
-
-	for (int i = 0;i<=GPS.size();i++) {
-
-		char letter_first = GPS[i].first.first;
-		char letter_second = GPS[i].second;
-		int flag = 0;
-
-		for (int j = i; j <= GPS.size(); j++) {
-			if (letter_first == GPS[j].first.first && flag > 0) {
-				GPS[j].second = alphabet[0];
-				gramG.push_back(alphabet[0]);
-				alphabet.erase(alphabet[0]);
-			}
-			else {
-				flag++; 
+	for (int i = 0; i <= gramG.size()-1; i++) {
+		for (int j = 0; j <= (alphabet.size() - gramG.size()+1); j++) {
+			if (gramG[i] == alphabet[j]) {
+				alphabet.erase(j, 1);
 			}
 		}
 	}
 
+	for (int i = 0;i<=GPS.size()-1;i++) {
+
+		char letter_first = GPS[i].first.first;
+		char transfer = GPS[i].second;
+		char letter_second  = GPS[i].first.second;
+
+		for (int j = i+1; j <= GPS.size()-1; j++) {
+			char previos1 = letter_second;
+			char previos2 = GPS[j].first.second;
+			char alph = alphabet[0];
+
+			if (letter_first == GPS[j].first.first && transfer == GPS[j].second) {
+
+				GPS[i].first.second = alphabet[0];
+				alphabet.erase(0, 1);
+				GPS.erase(GPS.begin() + j);
+
+
+				for (int l = 0; l <= GPS.size() - 1; l++) {
+					if (GPS[l].first.second == previos2 || GPS[l].first.second == previos1) {
+						GPS[l].first.second = alph;
+						cout << "pen \n";
+					}
+					if (GPS[l].first.first == previos1 || GPS[l].first.first == previos2) {
+						GPS[l].first.first = alph;
+						cout << "pen \n";
+					}
+				}
+			}
+		}
+	}
+			
+	
+	
+
 	for (int i = 0; i <= GPS.size() - 1; i++) {
-		cout << GPS[i].first.first << " -> " << GPS[i].second << " -> " << GPS[i].first.second << " \n";
+		cout << "\n" << GPS[i].first.first << " -> " << GPS[i].second << " -> " << GPS[i].first.second << " \n";
 	}
 
 	return 0;
 }
-
-
-
-
-
-
-
-
-	/*
-	for (int i = 0; i <= gramP.size(); i++) {
-		char first_letter = GPS[i].first.first;
-		char second_letter = GPS[i].first.second;
-
-		int flag = 0;
-		for (int j = 0; j <= gramP.size(); j++) {
-			if (second_letter == GPS[j].first.first)
-				flag = 1;
-				continue;
-			
-		}
-		if (flag==0)
-			gramG.push_back('@');
-		//if (gram1[gram1.size() - 1] == gramP[i])
-			//gramG.push_back('@');
-	}
-	*/
